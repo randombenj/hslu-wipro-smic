660533e49bb7d12a26a327d0d31511773429b5bd
Jonas Wyss <jonaswyss@gmail.com>
2021-10-08 11:16:24 +0200

    add /db/setDefaults endpoint to load a default dataset into the db

--- api/api/__main__.py
+++ api/api/__main__.py
@@ -1,7 +1,8 @@
+from sqlite3.dbapi2 import Timestamp
 from fastapi import FastAPI
 from sqlmodel import SQLModel, create_engine, Session
-
-from datastore.model import Meter, Metric
+from datetime import datetime
+from datastore.model import Meter, Measurement
 
 
 SQLITE_FILE_NAME = "database.db"
@@ -17,6 +18,7 @@ def create_db_and_tables():
 
 app = FastAPI()
 
+
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
@@ -28,11 +30,35 @@ def read_usage():
         meters = session.query(Meter).all()
         return meters
 
+
 @app.get("/meters/{meter_id}/")
 def read_meter(meter_id: int):
     with Session(engine) as session:
         meter = session.query(Meter).filter(Meter.id == meter_id).first()
         return meter
+
+
+@app.get("/meters/{meter_id}/measurements")
+def read_measurements(meter_id: int):
+    with Session(engine) as session:
+        measurements = session.query(Measurement).filter(Measurement.meter_id == meter_id).all()
+        return measurements
+
+
+@app.post("/db/setDefaults")
+def set_defaults():
+    with Session(engine) as session:
+        session.query(Meter).delete()
+        session.query(Measurement).delete()
+        meter1 = Meter(serial_number="Meter one", id=1)
+        meter2 = Meter(serial_number="Meter two", id=2)
+        measurement1 = Measurement(meter_id=meter1.id, voltage_phase_1=10,
+                                   voltage_phase_2=20, voltage_phase_3=30, capture_time=datetime.now())
+        session.add(meter1)
+        session.add(meter2)
+        session.add(measurement1)
+        session.commit()
+
 
 if __name__ == "__main__":
     import uvicorn
--- datastore/datastore/model.py
+++ datastore/datastore/model.py
@@ -1,15 +1,15 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
-
+from datetime import datetime
 
 class Meter(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     serial_number: str
 
 
-class Metric(SQLModel, table=True):
+class Measurement(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-
+    capture_time: datetime
     voltage_phase_1: float
     voltage_phase_2: float
     voltage_phase_3: float
