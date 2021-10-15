f4b3e9425a6186fda48bc9922650893cfa185316
Jonas Wyss <jonaswyss@gmail.com>
2021-10-15 13:11:06 +0200

    improve error handling and hooks

--- web/src/components/voltageGraph.tsx
+++ web/src/components/voltageGraph.tsx
@@ -1,37 +1,37 @@
-import useFetch from "use-http";
 import { Data } from "../data";
 import LineChart from "./lineChart";
-import {useGetMeasurements} from '../hooks/meters'
+import { useGetMeasurements } from '../hooks/meters'
 const VoltageGraph = () => {
     const {
         loading,
         error,
         data,
     } = useGetMeasurements(1);
+    if (data) {
+        const ddata = new Data(data);
+        console.log(ddata);
+        return (
+            <LineChart
+                svgProps={{
+                    margin: { top: 80, bottom: 80, left: 80, right: 80 },
+                    width: 400,
+                    height: 400,
+                }}
 
-    if (loading) return <p>Loading...</p>;
+                axisProps={{
+                    xLabel: 'Time',
+                    yLabel: 'Voltage',
+                }}
+                data={ddata}
+                strokeWidth={4}
+            ></LineChart>
+        )
+    }
+    else if (loading) return <p>Loading...</p>;
     else
         if (error) return <p>Error: {error.message}</p>;
-        else {
-            const ddata = new Data(data);
-            console.log(ddata);
-            return (
-                <LineChart
-                    svgProps={{
-                        margin: { top: 80, bottom: 80, left: 80, right: 80 },
-                        width: 400,
-                        height: 400,
-                    }}
+        else return <p>No data</p>;
 
-                    axisProps={{
-                        xLabel: 'Time',
-                        yLabel: 'Voltage',
-                    }}
-                    data={ddata}
-                    strokeWidth={4}
-                ></LineChart>
-            )
-        }
 }
 
 export default VoltageGraph;
--- web/src/hooks/meters.tsx
+++ web/src/hooks/meters.tsx
@@ -2,12 +2,11 @@ import useFetch from "use-http";
 import { Measurement } from "../model/measurement";
 import { Meter } from "../model/meter";
 
-export function useGetMeasurements(meterId: number): { data: Measurement[], loading: boolean, error: any } {
-    const { data, loading, error } = useFetch(`/meters/${meterId}/measurements`, { method: "GET" }, []);
-    return { data, loading, error };
+export function useGetMeasurements(meterId: number) {
+    return useFetch<Measurement[]>(`/meters/${meterId}/measurements`, { method: "GET" }, []);
+
 }
 
-export function useGetMeters(): { data: Meter[], loading: boolean, error: any } {
-    const { data, loading, error } = useFetch("/meters", { method: "GET" }, []);
-    return { data, loading, error };
+export function useGetMeters() {
+    return useFetch<Meter[]>("/meters", { method: "GET" }, []);
 }
