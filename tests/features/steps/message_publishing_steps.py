f4554bfbbcebede735b62bda6ee66b5b2447da9d
Benj Fassbind <randombenj@gmail.com>
2021-10-22 16:03:23 +0200

    Fix timing issues

--- tests/features/steps/message_publishing_steps.py
+++ tests/features/steps/message_publishing_steps.py
@@ -7,6 +7,20 @@ from behave import given, when, then, step
 from fakemeter import SMICCLient
 
 
+def __await_data_request(url) -> requests.Response:
+    timeout = time.time() + 60  # seconds
+    while True:
+        response = requests.get(url)
+        assert response.ok
+
+        if len(response.json()) > 0:
+            return response
+
+        if time.time() > timeout:
+            raise TimeoutError("Timed out waiting or api to serve the expected data")
+
+
+
 @given("a smart meter client")
 def step_get_client(context):
     context.client = SMICCLient()
@@ -26,24 +40,10 @@ def step_publish_data(context):
 
 @then("the data is available via the api")
 def step_data_is_available(context):
-
-    timeout = time.time() + 60  # seconds
-    while True:
-        response = requests.get("http://localhost:8080/meters")
-        assert response.ok
-
-        if len(response.json()) > 0:
-            break
-
-        if time.time() > timeout:
-            raise TimeoutError("Timed out waiting or api to serve the expected data")
-
-    meter_data = response.json()[0]
+    meter_data = __await_data_request("http://localhost:8080/meters").json()[0]
     assert meter_data["serial_number"] == context.data["meter_serial"]
 
-    response = requests.get(f"http://localhost:8080/meters/{meter_data['id']}/measurements")
-    assert response.ok
-    measurement_data = response.json()[0]
+    measurement_data = __await_data_request(f"http://localhost:8080/meters/{meter_data['id']}/measurements").json()[0]
     assert measurement_data["voltage_phase_1"] == context.data["voltage_phase_1"]
     assert measurement_data["voltage_phase_2"] == context.data["voltage_phase_2"]
     assert measurement_data["voltage_phase_3"] == context.data["voltage_phase_3"]
