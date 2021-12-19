7472e9db68f30dabcddd28123944ef4450779794
Benj Fassbind <randombenj@gmail.com>
2021-12-19 13:44:57 +0100

    Add TLS test

--- tests/features/steps/test_publish_message.py
+++ tests/features/steps/test_publish_message.py
@@ -1,5 +1,9 @@
 import json
 import pytest
+import socket
+import subprocess
+
+from pathlib import Path
 
 from pytest_bdd import scenario, given, when, then
 
@@ -41,3 +45,37 @@ def data_is_available(data, await_data_request):
     assert measurement_data["voltage_phase_1"] == data["voltage_phase_1"]
     assert measurement_data["voltage_phase_2"] == data["voltage_phase_2"]
     assert measurement_data["voltage_phase_3"] == data["voltage_phase_3"]
+
+
+@scenario("../publish_messages.feature", "Use TLS to secure connection")
+def test_tls_encryption():
+    pass
+
+
+@given("the mqtt endpoint is running")
+def mqtt_endpoint():
+    outcome = socket.socket(
+        socket.AF_INET, socket.SOCK_STREAM
+    ).connect_ex(("localhost", 8883))
+
+    assert outcome == 0
+
+
+@pytest.fixture
+@when("we check for tls encryption")
+def tls_encryption():
+    outcome = subprocess.run(
+        ["openssl", "s_client", "-connect", "localhost:8883", "-showcerts"],
+        capture_output=True
+    )
+
+    assert outcome.returncode == 0
+
+    return outcome.stdout.decode("utf-8")
+
+
+@then("the correct certificate is used")
+def correct_certificate_is_used(tls_encryption):
+    root_ca = Path(__file__).parent / ".." / ".." / ".." / "data" / "mkcert" / "rootCA.pem"
+    assert root_ca.exists()
+    assert root_ca.read_text() in tls_encryption
